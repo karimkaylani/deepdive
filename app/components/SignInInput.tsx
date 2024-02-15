dcd1f2131a1b9a70f068d8c5bca27be5bc93f904
karimkaylani <karimkaylani@gmail.com>
2024-02-15 10:54:09 -0800

    Fix typo

--- app/components/SignInInput.tsx
+++ app/components/SignInInput.tsx
@@ -29,9 +29,9 @@ export default function SignInInput() {
     return (
         <Stack align='center' className='mt-14' gap='lg'>
             <Title order={2}>Sign in</Title>
-            <Text style={{maxWidth: '480px'}} ta='center'>Sign in with your email to gain access to the playlist. You must have use the same email that has a Premium tier subscription to <Anchor href='https://www.deepdivenewsletter.com/'>The Deep Dive newsletter</Anchor></Text>
+            <Text style={{maxWidth: '450px'}} ta='center'>Sign in with your email to gain access to the playlist. You must use the same email that has a Premium tier subscription to <Anchor href='https://www.deepdivenewsletter.com/'>The Deep Dive newsletter</Anchor></Text>
             <TextInput type='email' error={error} value={email} onChange={(event) => setEmail(event.target.value)} 
-                style={{maxWidth: '480px'}} w={{base: '65%', sm: '50%', md: '35%', lg: '%20'}} 
+                style={{maxWidth: '450px'}} w={{base: '65%', sm: '50%', md: '35%', lg: '%20'}} 
                 label="Your email" leftSection={<IconAt />} />
             <Button size='lg' radius='lg' variant="filled" style={{backgroundColor: secondaryColor}}
                 onClick={handleSignInClick}>
