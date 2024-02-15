ba43fb2b7ee7771981b65b1e8062cc3be22c76a9
karimkaylani <karimkaylani@gmail.com>
2024-02-15 08:49:22 -0800

    Fix typo

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <Stack align='center' className='mt-14' gap='lg'>
             <Title order={2}>Sign in</Title>
-            <Text>An email with a sign in link has been sent to your account. Please check your inbox</Text>
+            <Text>An email with a sign in link has been sent to your account. Please check your inbox.</Text>
         </Stack>
     )
 }
