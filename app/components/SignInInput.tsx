3c3c30bd856d0baa12051f80c710e39c6c96843f
karimkaylani <karimkaylani@gmail.com>
2024-02-14 20:17:30 -0800

    Fix email input width

--- app/components/SignInInput.tsx
+++ app/components/SignInInput.tsx
@@ -31,7 +31,9 @@ export default function SignInInput() {
         <Stack align='center' className='mt-14' gap='lg'>
             <Title order={2}>Sign in</Title>
             <Text style={{maxWidth: '500px'}} ta='center'>Sign in with your email to gain access to the playlist. You must have use the same email that has a Premium tier subscription to The Deep Dive newsletter</Text>
-            <TextInput type='email' error={error} value={email} onChange={(event) => setEmail(event.target.value)} w={{base: '65%', sm: '50%', md: '35%'}} label="Your email" leftSection={<IconAt />} />
+            <TextInput type='email' error={error} value={email} onChange={(event) => setEmail(event.target.value)} 
+                style={{maxWidth: '500px'}} w={{base: '65%', sm: '50%', md: '35%', lg: '%20'}} 
+                label="Your email" leftSection={<IconAt />} />
             <Button size='lg' radius='lg' variant="filled" style={{backgroundColor: secondaryColor}}
                 onClick={handleSignInClick}>
                 Sign In
