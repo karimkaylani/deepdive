4e7fc09223b4c8c88c1407f76884158a7bff1adf
karimkaylani <karimkaylani@gmail.com>
2024-02-15 15:00:37 -0800

    Added not authorized message for API endpoint

--- app/api/refresh-videos/route.ts
+++ app/api/refresh-videos/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
     const session = await getServerSession(authOptions);
     const adminEmails = process.env.ADMIN_EMAILS.split(',')
     if (!session || !session.user || !adminEmails.includes(session.user.email)) {
-        redirect('/')
+        return NextResponse.json({message: "Not authorized", success: false})
     }
     revalidateTag('videos')
     return NextResponse.json({message: 'Updated video catalog', success: true})
